34f3f331a2c326861c2202d32834ec2104a8cdc6
francesca <francesca.giammichele@etiqa.it>
2023-06-16 08:56:47 +0200

    fix header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="md:basis-1/5 ">
+      <div className="md:basis-1/4 ">
         <NavLink to="/">
           <img alt="header" src={headerImage} width={300} height={300} />
         </NavLink>
       </div>
-      <div className="py-5 md:py-0 md:basis-4/5 flex items-center bg-blue-900">
+      <div className="py-5 md:py-0 md:basis-3/4 flex items-center bg-blue-900">
         <Navbar />
       </div>
     </div>
