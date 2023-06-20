8f1d706f20ccabcef0122d6a6ac0595abc9c7791
francesca <francesca.giammichele@etiqa.it>
2023-06-20 08:02:22 +0200

    fix navbar round style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import Navbar from "./Navbar";
 import headerImage from "../assets/header.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
+  let { pathname } = useLocation();
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className="md:basis-1/4 ">
@@ -10,7 +12,11 @@ const Header = () => {
           <img alt="header" src={headerImage} width={300} height={300} />
         </NavLink>
       </div>
-      <div className="py-5 md:py-0 md:basis-3/4 flex items-center bg-color1 md:rounded-bl-lg">
+      <div
+        className={`py-5 md:py-0 md:basis-3/4 flex items-center bg-color1 ${
+          pathname === "/" ? "" : "md:rounded-bl-lg"
+        } `}
+      >
         <Navbar />
       </div>
     </div>
