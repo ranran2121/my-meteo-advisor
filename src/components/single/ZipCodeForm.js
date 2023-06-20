f46ea4c5eb605f20dba8edf9c8d3463e796c49b6
francesca <francesca.giammichele@etiqa.it>
2023-06-20 07:13:44 +0200

    fix some style

--- src/components/single/ZipCodeForm.js
+++ src/components/single/ZipCodeForm.js
@@ -68,80 +68,85 @@ const LocationForm = () => {
   };
 
   return (
-    <form
-      onSubmit={handleOnSubmit}
-      className="px-8 w-full flex flex-col justify-center"
-    >
-      <div className="flex flex-col">
-        <label
-          className="my-4 text-color4 font-semibold"
-          htmlFor="search-zipCode"
-        >
-          ZipCode
-        </label>
-        <input
-          className="px-4 py-2 rounded-full text-color1"
-          id="search-zipCode"
-          aria-label="Search zipCode"
-          placeholder="Search"
-          type="search"
-          value={zipCode}
-          onChange={(e) => {
-            setError(false);
-            let invalid = { ...invalidInput, zip: "" };
-            setInvalidInput(invalid);
-            const reg = /^\d+$/;
-            if (!reg.test(e.target.value)) {
-              invalid = { ...invalidInput, zip: "invalid input" };
-              setInvalidInput(invalid);
-            }
-            setZipCode(e.target.value);
-          }}
-        />
-        {invalidInput.zip && (
-          <span className="input-error">{invalidInput.zip}</span>
-        )}
-      </div>
-      <div className="flex flex-col">
-        <label
-          className="my-4 text-color4 font-semibold"
-          htmlFor="search-zipCode"
-        >
-          CountryCode
-        </label>
-        <input
-          className="px-4 py-2 rounded-full uppercase text-color1"
-          id="search-countryCode"
-          aria-label="Search countryCode"
-          placeholder="e.g. IT"
-          type="search"
-          value={countryCode}
-          onChange={(e) => {
-            setError(false);
-            let invalid = { ...invalidInput, country: "" };
-            setInvalidInput(invalid);
-            if (e.target.value.length > 2 || e.target.value.length < 2) {
-              invalid = { ...invalidInput, country: "invalid input" };
-              setInvalidInput(invalid);
-            }
-            setCountryCode(e.target.value);
-          }}
-        />
-        {invalidInput.country && (
-          <span className="input-error">{invalidInput.country}</span>
-        )}
-      </div>
-      <button
-        type="submit"
-        className="text-color4 font-semibold uppercase my-4 rounded-full px-4 py-2 bg-color3"
+    <div className="mt-4 text-color4 w-full px-8">
+      <h2 className="text-xl font-semibold text-color3">
+        Search by Zip/Country code
+      </h2>
+      <form
+        onSubmit={handleOnSubmit}
+        className="w-full flex flex-col justify-center"
       >
-        {!isLoading ? (
-          "search"
-        ) : (
-          <BeatLoader color="#fff" speedMultiplier={1} />
-        )}
-      </button>
-    </form>
+        <div className="flex flex-col">
+          <label
+            className="my-4 text-color4 font-semibold"
+            htmlFor="search-zipCode"
+          >
+            ZipCode
+          </label>
+          <input
+            className="px-4 py-2 rounded-full text-color1"
+            id="search-zipCode"
+            aria-label="Search zipCode"
+            placeholder="Search"
+            type="search"
+            value={zipCode}
+            onChange={(e) => {
+              setError(false);
+              let invalid = { ...invalidInput, zip: "" };
+              setInvalidInput(invalid);
+              const reg = /^\d+$/;
+              if (!reg.test(e.target.value)) {
+                invalid = { ...invalidInput, zip: "invalid input" };
+                setInvalidInput(invalid);
+              }
+              setZipCode(e.target.value);
+            }}
+          />
+          {invalidInput.zip && (
+            <span className="input-error">{invalidInput.zip}</span>
+          )}
+        </div>
+        <div className="flex flex-col">
+          <label
+            className="my-4 text-color4 font-semibold"
+            htmlFor="search-zipCode"
+          >
+            CountryCode
+          </label>
+          <input
+            className="px-4 py-2 rounded-full uppercase text-color1"
+            id="search-countryCode"
+            aria-label="Search countryCode"
+            placeholder="e.g. IT"
+            type="search"
+            value={countryCode}
+            onChange={(e) => {
+              setError(false);
+              let invalid = { ...invalidInput, country: "" };
+              setInvalidInput(invalid);
+              if (e.target.value.length > 2 || e.target.value.length < 2) {
+                invalid = { ...invalidInput, country: "invalid input" };
+                setInvalidInput(invalid);
+              }
+              setCountryCode(e.target.value);
+            }}
+          />
+          {invalidInput.country && (
+            <span className="input-error">{invalidInput.country}</span>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="text-color4 font-semibold uppercase my-4 rounded-full px-4 py-2 bg-color3"
+        >
+          {!isLoading ? (
+            "search"
+          ) : (
+            <BeatLoader color="#fff" speedMultiplier={1} />
+          )}
+        </button>
+      </form>
+    </div>
   );
 };
 
