b64706857cdd5b76becf209fa1225bbc8497878b
francesca <francesca.giammichele@etiqa.it>
2023-06-20 08:01:09 +0200

    fix sidebar style

--- src/components/single/SearchLocation.js
+++ src/components/single/SearchLocation.js
@@ -43,8 +43,8 @@ const SearchLocation = ({ setCities }) => {
   };
 
   return (
-    <div className="mt-4 text-color4">
-      <h2 className="text-xl font-semibold">Search by Location</h2>
+    <div className="mt-4 text-color4 w-full px-8">
+      <h2 className="text-xl font-semibold text-color5">Search by Location</h2>
       <form
         onSubmit={handleOnSubmitForm}
         className="w-full flex flex-col justify-center"
@@ -69,7 +69,7 @@ const SearchLocation = ({ setCities }) => {
         )}
         <button
           type="submit"
-          className="text-color4 font-semibold uppercase my-4 rounded-full px-4 py-2 bg-color3"
+          className="text-color4 font-semibold uppercase my-4 rounded-full px-4 py-2 bg-color5"
         >
           {!isLoading ? (
             "search"
