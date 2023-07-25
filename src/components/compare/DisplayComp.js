f4a263293207c1bc09a5fc2b224b29c0f69ce200
francesca <francesca.giammichele@etiqa.it>
2023-07-25 16:14:19 +0200

    wip

--- src/components/compare/DisplayComp.js
+++ src/components/compare/DisplayComp.js
@@ -10,20 +10,7 @@ import axios from "axios";
 
 const DisplayComp = () => {
   const [hasWinner, setHasWinner] = useState("");
-  const {
-    data,
-    error,
-    isLoading,
-    setIsLoading,
-    setData,
-    setError,
-    searchParams,
-  } = useContext(CompareContext);
-
-  const lat1 = searchParams.get("lat1");
-  const lon1 = searchParams.get("lon1");
-  const lat2 = searchParams.get("lat2");
-  const lon2 = searchParams.get("lon2");
+  const { data, error } = useContext(CompareContext);
 
   const i = findIndex();
 
@@ -38,103 +25,72 @@ const DisplayComp = () => {
     }
   };
 
-  const loaderLoc = async () => {
-    if (!lat1 || !lat2 || !lon1 || !lon2) {
-      setIsLoading(false);
-      return;
-    }
-    try {
-      //call openwathermap geolocation api to retrieve lat&lon coordinates
-      const URL1 = `${WEATHER_API_BASEURL}/data/2.5/forecast?lat=${lat1}&lon=${lon1}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
-      const forecast1 = await axios.get(URL1);
-
-      const URL2 = `${WEATHER_API_BASEURL}/data/2.5/forecast?lat=${lat2}&lon=${lon2}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
-      const forecast2 = await axios.get(URL2);
-
-      setData({ forecast1: forecast1.data, forecast2: forecast2.data });
-    } catch (e) {
-      setData(null);
-      setError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (lat1 && lon1 && lat2 && lon2) {
-      loaderLoc();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lat1, lon1, lat2, lon2, isLoading]);
-
   if (error) {
     return <Error />;
   }
 
-  if (data) {
-    return (
-      <div className="text-center flex flex-col items-center mb-4">
-        <div className="py-4 px-4">
-          <h2 className="text-color1 text-2xl font-bold">
-            This is the weather forecast for
-          </h2>
-          <h1 className="mt-2 text-color2 font-extrabold text-3xl">
-            {data.forecast1.city.name}
-          </h1>
-          <h2 className="text-color1 text-2xl font-bold mt-2">vs</h2>
-          <h1 className="mt-2 text-color2 font-extrabold text-3xl">
-            {data.forecast2.city.name}
-          </h1>
-        </div>
+  return (
+    <div className="text-center flex flex-col items-center mb-4">
+      <div className="py-4 px-4">
+        <h2 className="text-color1 text-2xl font-bold">
+          This is the weather forecast for
+        </h2>
+        <h1 className="mt-2 text-color2 font-extrabold text-3xl">
+          {data.forecast1.city.name}
+        </h1>
+        <h2 className="text-color1 text-2xl font-bold mt-2">vs</h2>
+        <h1 className="mt-2 text-color2 font-extrabold text-3xl">
+          {data.forecast2.city.name}
+        </h1>
+      </div>
 
-        <div className="border-b-2 border-color1 w-[70%] my-4"></div>
+      <div className="border-b-2 border-color1 w-[70%] my-4"></div>
 
-        <div className="mt-4">
-          <div className="flex flex-row gap-1 lg:gap-2">
-            <div className="basis-1/3">
-              <div className="flex flex-col rounded-xl  px-1 md:px-3 border-2 border-color4">
-                <div className="displayColEntry font-bold uppercase text-sm"></div>
-              </div>
-            </div>
-            <div className="basis-1/3">
-              <SingleBlock city={data.forecast1.city.name} />
-            </div>
-            <div className="basis-1/3">
-              <SingleBlock city={data.forecast2.city.name} />
+      <div className="mt-4">
+        <div className="flex flex-row gap-1 lg:gap-2">
+          <div className="basis-1/3">
+            <div className="flex flex-col rounded-xl  px-1 md:px-3 border-2 border-color4">
+              <div className="displayColEntry font-bold uppercase text-sm"></div>
             </div>
           </div>
-
-          <div className="mt-2 flex flex-row gap-1 lg:gap-2">
-            <div className="basis-1/3">
-              <LegendColumn />
-            </div>
-            <div className="basis-1/3">
-              <DataColumn data={data.forecast1.list[i]} />
-            </div>
-            <div className="basis-1/3">
-              <DataColumn data={data.forecast2.list[i]} />
-            </div>
+          <div className="basis-1/3">
+            <SingleBlock city={data.forecast1.city.name} />
+          </div>
+          <div className="basis-1/3">
+            <SingleBlock city={data.forecast2.city.name} />
           </div>
         </div>
 
-        <div className="mt-6">
-          {!hasWinner ? (
-            <button
-              type="button"
-              className="bg-color1 rounded-full px-4 py-2 text-color4 uppercase"
-              onClick={handleOnclick}
-            >
-              ...and the winner is?
-            </button>
-          ) : (
-            <div className="text-color1 uppercase font-extrabold border-4 border-color1 bg-color5 px-4 py-2 rounded-full">
-              {hasWinner}
-            </div>
-          )}
+        <div className="mt-2 flex flex-row gap-1 lg:gap-2">
+          <div className="basis-1/3">
+            <LegendColumn />
+          </div>
+          <div className="basis-1/3">
+            <DataColumn data={data.forecast1.list[i]} />
+          </div>
+          <div className="basis-1/3">
+            <DataColumn data={data.forecast2.list[i]} />
+          </div>
         </div>
       </div>
-    );
-  }
+
+      <div className="mt-6">
+        {!hasWinner ? (
+          <button
+            type="button"
+            className="bg-color1 rounded-full px-4 py-2 text-color4 uppercase"
+            onClick={handleOnclick}
+          >
+            ...and the winner is?
+          </button>
+        ) : (
+          <div className="text-color1 uppercase font-extrabold border-4 border-color1 bg-color5 px-4 py-2 rounded-full">
+            {hasWinner}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default DisplayComp;
--- src/components/compare/SearchLocationComp.js
+++ src/components/compare/SearchLocationComp.js
@@ -1,36 +1,25 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect, useCallback } from "react";
 import { CompareContext } from "../../routes/Compare";
 import { WEATHER_API_BASEURL } from "../../constants";
 import axios from "axios";
 import BeatLoader from "react-spinners/BeatLoader";
 
 const SearchLocationComp = ({ setCities }) => {
-  const [location1, setLocation1] = useState("");
-  const [location2, setLocation2] = useState("");
+  const {
+    errorSearch,
+    setErrorSearch,
+    isLoading,
+    setIsLoading,
+    setSearchParams,
+    searchParams,
+  } = useContext(CompareContext);
+  const loc1 = searchParams.get("loc1");
+  const loc2 = searchParams.get("loc2");
+  const [location1, setLocation1] = useState(loc1 ?? "");
+  const [location2, setLocation2] = useState(loc2 ?? "");
   const [invalidInput, setInvalidInput] = useState({ loc1: "", loc2: "" });
-  const { errorSearch, setErrorSearch, isLoading, setIsLoading } =
-    useContext(CompareContext);
 
-  const handleOnSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!location1 && !location2) {
-      setInvalidInput({ loc1: "input is required", loc2: "input is required" });
-      return;
-    }
-
-    if (!location1) {
-      setInvalidInput({ loc1: "input is required" });
-      return;
-    }
-
-    if (!location2) {
-      setInvalidInput({ loc2: "input is required" });
-      return;
-    }
-
-    setIsLoading(true);
-
+  const loaderCities = useCallback(async () => {
     try {
       const URL1 = `${WEATHER_API_BASEURL}/geo/1.0/direct?q=${location1}&limit=5&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
       const resp1 = await axios.get(URL1);
@@ -64,7 +53,35 @@ const SearchLocationComp = ({ setCities }) => {
     } finally {
       setIsLoading(false);
     }
+  }, [location1, location2, setCities, setErrorSearch, setIsLoading]);
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!location1 && !location2) {
+      setInvalidInput({ loc1: "input is required", loc2: "input is required" });
+      return;
+    }
+
+    if (!location1) {
+      setInvalidInput({ loc1: "input is required" });
+      return;
+    }
+
+    if (!location2) {
+      setInvalidInput({ loc2: "input is required" });
+      return;
+    }
+
+    setIsLoading(true);
+    setSearchParams({ loc1: location1, loc2: location2 });
   };
+
+  useEffect(() => {
+    if (loc1 && loc2) {
+      loaderCities();
+    }
+  }, [loaderCities, loc1, loc2]);
 
   return (
     <div className="my-4 text-color4">
