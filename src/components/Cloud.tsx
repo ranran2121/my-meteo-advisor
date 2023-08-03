20490002ab407fcd080a8f50e48e58ab2bf166f6
francesca <francesca.giammichele@etiqa.it>
2023-08-03 09:38:36 +0200

    fix style

--- src/components/Cloud.tsx
+++ src/components/Cloud.tsx
@@ -7,7 +7,7 @@ const Cloud: FC<CloudType> = ({ src, text, link, alt, left }) => {
   return (
     <div
       className={`cloud relative 
-     hover:scale-110 shadow-2xl ${left ? "self-end" : ""} mb-20 md:mb-0`}
+     hover:scale-110 shadow-2xl ${left ? "self-end" : ""} mb-20 lg:mb-0`}
     >
       <div className="absolute top-16 left-0 md:top-12 md:left-6 flex flex-row z-10">
         <img
@@ -15,7 +15,7 @@ const Cloud: FC<CloudType> = ({ src, text, link, alt, left }) => {
           alt={alt}
           className="pl-4 md:pl-6 basis-1/4 w-[100px] h-[60px] md:w-[120px] md:h-[80px]"
         />
-        <div className="basis-3/4 text-color1 text-sm md:text-lg ml-4 px-2">
+        <div className="basis-3/4 text-color1 text-sm md:text-lg ml-4 px-2 md:px-4">
           <p className="inline">{text}</p>
           <Link to={link}>
             <ArrowRightIcon className="h-6 w-6 ml-4 inline text-color5 animate-pulse hover:scale-150" />
