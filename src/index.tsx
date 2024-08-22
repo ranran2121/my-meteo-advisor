4d76efef06c375657347ba2eb5122230c1f83dae
francesca <francesca.giammichele@etiqa.it>
2024-08-22 10:43:25 +0200

    add query client

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,16 @@ import "./style/index.css";
 import { RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import router from "./routing";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
